b10cb6c37ab15d78efd2b061eccc3deacb804d89
Vaishnavi Rai <vaishnavirai2403@gmail.com>
2025-06-19 07:58:47 +0530

    testing

--- src/ex_01_java_basics/Lab001.java
+++ src/ex_01_java_basics/Lab001.java
@@ -1,4 +1,12 @@
 package ex_01_java_basics;
 
 public class Lab001 {
+    public static void main(String[] args) {
+        int _name = 10;
+        if(_name == 10){
+
+        }
+        System.out.println("hellow world");
+
+    }
 }
